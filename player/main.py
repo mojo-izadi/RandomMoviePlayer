87cc1896dbbf52425d10baf4f0660919b901262e
mojo-izadi <80407427+mojo-izadi@users.noreply.github.com>
2023-01-25 14:36:07 +0330

    adding support for nested directories

--- player/main.py
+++ player/main.py
@@ -3,17 +3,30 @@ import os
 import random
 import subprocess
 
+def getAllPlayableFiles(path):
+    to_return = []
+    all_files = [os.path.join(path, x) for x in os.listdir(path)]
+    to_return += [x for x in all_files if isPlayableFile(x)]
+    all_folders = [x for x in all_files if not (os.path.isfile(x))]
+
+    for folder in all_folders:
+        to_return += getAllPlayableFiles(folder)
+
+    return to_return
+
+def isPlayableFile(file_path):
+    return file_path.endswith(".mkv") | file_path.endswith(".mp4") | file_path.endswith(".webm")
+
 file = open('paths.json')
 path_data = json.load(file)
 movie_directory_path = path_data['movie_directory_path']
 media_player_path = path_data['media_player_path']
 file.close()
 
+movies = getAllPlayableFiles(movie_directory_path)
 
-movie = random.choice(os.listdir(movie_directory_path))
-movie_path = os.path.join(movie_directory_path, movie)
-movie_file_name = [x for x in os.listdir(movie_path) if x.endswith(".mkv") | x.endswith(".mp4")][0]
-movie_file_path = os.path.join(movie_path, movie_file_name)
+movie = random.choice(movies)
+print(movie)
 
-subprocess.Popen([media_player_path, movie_file_path])
+subprocess.Popen([media_player_path, movie])
 
